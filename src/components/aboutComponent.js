bd619763ca9705fc6379fb0fb722f7b1f42c12cc
davidtsai130 <davidtsai130@gmail.com>
2016-10-24 00:27:11 -0400

    Changed aboutcomponent to use react-bootstrap, added state to about container for skills instead of hardcoding them in aboutcomponent

--- src/components/aboutComponent.js
+++ src/components/aboutComponent.js
@@ -1,50 +1,67 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
 
 class AboutComponent extends Component {
+
+  handleSkills(column) {
+    return (column.map( (skill, idx) => {
+        return <li key={idx}>{skill}</li>;
+      })
+    )
+  }
+
   render() {
     return (
       <div>
         <h1> about me </h1>
           <br/>
-        <div className="row">
-          <div className="col-sm-12 col-md-6 col-lg-6">
-            <p>
-              After graduating from Rutgers Business School in May 2016 with a B.S. in Business Analytics and Information Technology, I went on to complete the Web Development Immersive course at <a href="https://flatironschool.com/" target="_blank">Flatiron School</a> in August 2016.
-            </p>
-          </div>
-          <div className="col-sm-12 col-md-6 col-lg-6">
-            <p>
-              I&#39;m a full stack web developer that is passionate about learning and implementing new technologies. I specialize in Rails and React JS/Redux. Check out <Link to="/portfolio"> my portfolio </Link> to see some of the web applications I have worked on and developed.
-            </p>
-          </div>
-        </div>
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-          <h3>Skills</h3>
-          <ul>
-            <li>CSS3</li>
-            <li>HTML5</li>
-            <li>Javascript</li>
-            <li>Jquery</li>
-            <li>MySql</li>
-            <li>Postgres</li>
-            <li>Rails</li>
-            <li>React</li>
-            <li>Redux</li>
-            <li>Ruby</li>
-            <li>Sinatra</li>
-          </ul>
-        </div>
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-          <h3> Interests </h3>
-          <ul>
-            <li>cars and motorcycles</li>
-            <li>digital and film photography</li>
-            <li>snowboarding</li>
-            <li>video games</li>
-            <li>DIY projects</li>
-          </ul>
-        </div>
+        <Grid id="about_me">
+          <Row>
+            <Col xs={12} sm={6} md={6} lg={6}>
+              <p>
+                After graduating from Rutgers Business School in May 2016 with a B.S. in Business Analytics and Information Technology, I went on to complete the Web Development Immersive course at <a href="https://flatironschool.com/" target="_blank">Flatiron School</a> in August 2016.
+              </p>
+            </Col>
+            <Col xs={12} sm={6} md={6} lg={6}>
+              <p>
+                I&#39;m a full stack web developer that is passionate about learning and implementing new technologies. I specialize in Rails and React JS/Redux. Check out <Link to="/portfolio"> my portfolio </Link> to see some of the web applications I have worked on.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} sm={6} md={6} lg={6}>
+              <h3>Skills</h3>
+              <Row>
+                <Col id="first" xs={4} sm={4} md={4} lg={4}>
+                  <ul>
+                  {this.handleSkills(this.props.first)}
+                  </ul>
+                </Col>
+                <Col id="second" xs={4} sm={4} md={4} lg={4}>
+                <ul>
+                  {this.handleSkills(this.props.second)}
+                </ul>
+                </Col>
+                <Col id="third" xs={4} sm={4} md={4} lg={4}>
+                <ul>
+                  {this.handleSkills(this.props.third)}
+                </ul>
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12} sm={6} md={6} lg={6}>
+              <h3> Interests </h3>
+              <ul>
+                <li>cars and motorcycles</li>
+                <li>digital and film photography</li>
+                <li>snowboarding</li>
+                <li>video games</li>
+                <li>DIY projects</li>
+              </ul>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
--- src/containers/aboutContainer.js
+++ src/containers/aboutContainer.js
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 import AboutComponent from '../components/aboutComponent'
 
 class AboutContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {skills: ['CSS3', 'HTML5', 'Javascript', 'Jquery', 'MySQL',
+                    'Postgres', 'Rails', 'React', 'Redux', 'Ruby',
+                    'Sinatra'],
+                  skillOne: [],
+                  skillTwo: [],
+                  skillThree: []}
+  }
+
+  componentWillMount() {
+    this.organizeSkills()
+  }
+
+  organizeSkills() {
+    var arrayLength = this.state.skills.length
+
+    for (var i = 0; i < arrayLength; i++) {
+      var section = Math.ceil(arrayLength/3)
+      if (this.state.skillOne.length !== section) {
+        this.state.skillOne.push(this.state.skills[i])
+      } else if (this.state.skillTwo.length !== section){
+        this.state.skillTwo.push(this.state.skills[i])
+      } else {
+        this.state.skillThree.push(this.state.skills[i])
+      }
+    }
+  }
+
   render() {
     return (
       <div>
         <div className="container container-margin">
           <div className="row">
             <div className="col-lg-12 col-md-12 col-sm-12">
-              <AboutComponent />
+              <AboutComponent first={this.state.skillOne} second={this.state.skillTwo} third={this.state.skillThree}/>
             </div>
           </div>
         </div>
