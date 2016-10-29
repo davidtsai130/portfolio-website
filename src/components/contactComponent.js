e4fc99e3daf2170664198493c1cd1ea97ce700a9
davidtsai130 <davidtsai130@gmail.com>
2016-10-29 19:14:15 -0400

    added email info to contact page

--- src/components/contactComponent.js
+++ src/components/contactComponent.js
@@ -5,6 +5,9 @@ class contactComponent extends Component {
     return (
       <div>
         <h1> let&#39;s connect! </h1>
+        <br/>
+        <h3> Email: </h3>
+        <h4> david.tsai@rutgers.edu </h4>
       </div>
     )
   }
