824e7d78678b08c111b67aefb5e29e834295a3f4
davidtsai130 <davidtsai130@gmail.com>
2016-10-31 22:23:10 -0400

    added modal browser compatibility with Firefox

--- src/components/modalComponent.js
+++ src/components/modalComponent.js
@@ -9,8 +9,17 @@ const modalComponent = OnClickOutside(React.createClass({
   },
 
     handleClickOutside: function(instance) {
-      if (instance.path[1].className !== 'carousel slide' && instance.path[3].className !== 'modal-content' && instance.path[5].className !== 'modal-content' && instance.path[6].className !== 'modal-content') {
-        browserHistory.push('/portfolio')
+    /* CHROME */
+      if (!!window.chrome) {
+        if (instance.path[1].className !== 'carousel slide' && instance.path[3].className !== 'modal-content' && instance.path[5].className !== 'modal-content' && instance.path[6].className !== 'modal-content') {
+          browserHistory.push('/portfolio')
+        }
+      }
+    /* FIREFOX */
+      else if (typeof InstallTrigger !== 'undefined') {
+        if (instance.target.className == 'close' || instance.target.className == "modal-lg modal-dialog" || instance.target.className == "fade in modal" ) {
+          browserHistory.push('/portfolio')
+        }
       }
     },
 
